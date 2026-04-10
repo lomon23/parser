2338577bd93cd4d1a3bf63ea482e7723326fb334
lomon23 <lomontav22@gmail.com>
2026-04-10 15:26:56 +0300

    feat(log): add tabulate for txt log file

--- exporter.py
+++ exporter.py
@@ -1,19 +1,25 @@
+# Встанови: pip install tabulate
+from tabulate import tabulate
 import pandas as pd
-import os
 
 class Exporter:
     @staticmethod
     def to_csv(data: list, filename: str):
-        """Зберігає список словників у CSV файл."""
-        if not data:
-            raise ValueError("Немає даних для збереження.")
-            
-        # Гарантуємо, що файл буде з розширенням .csv
-        if not filename.endswith('.csv'):
-            filename += '.csv'
-            
         df = pd.DataFrame(data)
         df.to_csv(filename, index=False, encoding="utf-8-sig")
+        return filename
+
+    @staticmethod
+    def to_pretty_report(data: list, filename: str):
+        """Зберігає дані у вигляді красивої ASCII-таблиці (псевдо-таблиця)."""
+        if not data:
+            return None
+            
+        # Формуємо таблицю
+        # headers="keys" автоматично візьме назви з ключів словника
+        table = tabulate(data, headers="keys", tablefmt="grid")
         
-        filepath = os.path.abspath(filename)
-        return filepath
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(table)
+            
+        return filename
